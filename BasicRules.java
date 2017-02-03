011291c9f0c52e63c0227a8ef443f11ddff94d7b
Thomas Morton <Suitengo@gmail.com>
2017-02-03 14:35:40 -0800

    Just variables for gameplay

---
+++ BasicRules.java
@@ -0,0 +1,13 @@
+	Public class BasicRules {
+		//BasicRules used for basic setup for rules of netrunner
+		int Decksize = 45; //Keeps track of decksize if 0 hasLost = true
+		int CardsInHand = 5; //Keeps track of cards in hand
+		int AgendaPoints = 0; //Keeps track of Agenda points if 7 hasWon = true
+		int Credits = 5; //Keeps track of credits to see if you can play cards
+		int Clicks = 3; //keeps track of number of clicks left
+		
+		Boolean hasWon = false; //if true you WIN!
+		Boolean hasLost = false; //if true you LOSE!
+		Boolean isTurn = true; //it is your turn
+
+	}
