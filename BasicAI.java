d2b2596c4ecc3f564649705cd4ed48ee2dd920d6
Thomas Morton <Suitengo@gmail.com>
2017-02-09 08:45:54 -0800

    added to BasicAI plan for priorites

---
+++ BasicAI.java
@@ -0,0 +1,17 @@
+	public class BasicAI {
+		
+		public static void main (String[]args) {
+			if (BasicRules.isTurn) {
+				BasicRules.Clicks = 3;
+				BasicRules.CardsInHand += 1;
+				
+			}
+
+		}
+
+		//set up priorities
+		//first if you can win the game do so
+		//second if you can install ice do so
+		//thrid if you can gain credits do so
+		//fourth if you can score an agenda do so
+	}
